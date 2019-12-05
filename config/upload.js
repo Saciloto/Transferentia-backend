6f98c330b58b0ca397e5faa9e85fb44614e28cc3
Mauricio Valadares Saciloto <41171890+Saciloto@users.noreply.github.com>
2019-12-05 11:30:39 -0300

    Imagem

--- config/upload.js
+++ config/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
  //ASSIM FUNCIOA NO INSOMINIA MAS NÃO NO CELULAR
 module.exports = {
     storage: multer.diskStorage({
-        destination:path.resolve(__dirname,'..','uploads/resized'),
+        destination:path.resolve(__dirname,'..','uploads','resized'),
         filename:(req,file,cb) =>{
             cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
         }
